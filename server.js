97bcfddd82ef136225b050a846491d2318dae001
Andrew Taylor <andytaylor575@gmail.com>
2020-06-30 15:57:36 -0400

    committing all the files

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.use(express.static("public"));
+
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
